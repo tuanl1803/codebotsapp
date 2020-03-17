2c63d70bcca3f1db16d20fa25f301eaec4123b0b
Louis <levtuanit@gmail.com>
2020-03-18 00:08:30 +0700

    Created: - Fix code overwrite by bots

--- serverside/src/main/java/helloapp/configs/quartz/SchedulerConfig.java
+++ serverside/src/main/java/helloapp/configs/quartz/SchedulerConfig.java
@@ -1,6 +1,6 @@
 /*
  * @bot-written
- * 
+ *
  * WARNING AND NOTICE
  * Any access, download, storage, and/or use of this source code is subject to the terms and conditions of the
  * Full Software Licence as accepted by you before being granted access to this source code and other materials,
@@ -9,7 +9,7 @@
  * licence termination and further legal action, and be required to indemnify Codebots for any loss or damage,
  * including interest and costs. You are deemed to have accepted the terms of the Full Software Licence on any
  * access, download, storage, and/or use of this source code.
- * 
+ *
  * BOT WARNING
  * This file is bot-written.
  * Any changes out side of "protected regions" will be lost next time the bot makes any changes.
@@ -37,8 +37,9 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 
-// % protected region % [Add any additional imports here] off begin
+// % protected region % [Add any additional imports here] on begin
 import helloapp.jobs.SimpleJob;
+import helloapp.jobs.TankCheckJob;
 // % protected region % [Add any additional imports here] end
 
 @Configuration
--- serverside/src/main/java/helloapp/repositories/FishRepository.java
+++ serverside/src/main/java/helloapp/repositories/FishRepository.java
@@ -1,6 +1,6 @@
 /*
  * @bot-written
- * 
+ *
  * WARNING AND NOTICE
  * Any access, download, storage, and/or use of this source code is subject to the terms and conditions of the
  * Full Software Licence as accepted by you before being granted access to this source code and other materials,
@@ -9,7 +9,7 @@
  * licence termination and further legal action, and be required to indemnify Codebots for any loss or damage,
  * including interest and costs. You are deemed to have accepted the terms of the Full Software Licence on any
  * access, download, storage, and/or use of this source code.
- * 
+ *
  * BOT WARNING
  * This file is bot-written.
  * Any changes out side of "protected regions" will be lost next time the bot makes any changes.
@@ -68,6 +68,11 @@ public interface FishRepository extends AbstractRepository<FishEntity> {
 	 */
 	List<FishEntity> findByBorn(@NotNull BornEnum born);
 
-	// % protected region % [Add any additional class methods here] off begin
+	// % protected region % [Add any additional class methods here] on begin
+	default List findByAliveAndPurchased() {
+		QFishEntity fishEntity = QFishEntity.fishEntity;
+		Predicate predicate = fishEntity.alive.eq(true).and(fishEntity.born.eq(BornEnum.PURCHASED));
+		return Lists.newArrayList(this.findAll(predicate));
+	}
 	// % protected region % [Add any additional class methods here] end
 }
